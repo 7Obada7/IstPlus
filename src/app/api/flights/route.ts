8a797bb5ec779c8b29a94b5e254298460ab3385f
Obada Masri <obada.masri@stu.fsm.edu.tr>
2026-02-12 02:48:22 +0300

    line added

--- src/app/api/flights/route.ts
+++ src/app/api/flights/route.ts
@@ -1,7 +1,11 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
+
+
+
 
 export async function GET() {
     try {
